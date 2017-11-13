693ab6ad221174270793f238d6734e51a49f275a
akunovac <akunovac@avans.nl>
2017-11-13 14:44:49 +0100

    added views

--- Ninja/Ninja.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Ninja]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NOT NULL, 
-    [Gold] INT NOT NULL, 
-    [Strength] INT NOT NULL, 
-    [Intelligence] INT NOT NULL, 
-    [Agility] INT NOT NULL
-)
